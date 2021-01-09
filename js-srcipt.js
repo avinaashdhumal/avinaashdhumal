5e655393998eb5c119be5e328d97b69a3e78373e
Avinaash Dhumal <dhumalavinash1703@gmail.com>
2021-01-09 16:07:57 +0530

    addinng h2 fontcolor white for dark mode

--- js-srcipt.js
+++ js-srcipt.js
@@ -1,14 +1,21 @@
-
+dark()
 function light(){
     document.body.style.transitionDuration="backgroundColor 3s linear";
     document.body.style.backgroundColor="white";
     document.getElementById("light").style.display="none";
     document.getElementById("dark").style.display="inherit";
+    // document.getElementById("welcome").style.display="none";
 }
 function dark(){
     document.body.style.transitionDuration="backgroundColor 3s linear";
     document.body.style.backgroundColor="black";
     document.getElementById("light").style.display="inherit";
     document.getElementById("dark").style.display="none";
-    document.getElementsByTagName("li").backgroundColor="rgb(133, 84, 255)"
+    document.getElementsByTagName("li").backgroundColor="rgb(133, 84, 255)";
+    // document.getElementById("welcome").style.display="inherit";
+    var arr=document.querySelectorAll("h2");
+    arr.forEach(item=>{
+        item.style.color = "white";
+    })
+    console.log(arr);
 }
